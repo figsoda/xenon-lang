2716c75e03a53eb71ce1df1fda506fde411346b6
figsoda <figsoda@pm.me>
2020-09-01 12:40:16 -0400

    Separated abstract syntax tree into a new package

--- xenon-parser/src/Xenon/Parser.hs
+++ xenon-parser/src/Xenon/Parser.hs
@@ -21,7 +21,7 @@ import Text.Megaparsec.Char ( char, space1 )
 import Text.Megaparsec.Char.Lexer ( binary, decimal, float, hexadecimal, octal
                                   , skipBlockComment, skipLineComment, space )
 import Text.Megaparsec.Error ( ErrorItem(..) )
-import Text.Printf ( printf )
+import Xenon.Ast ( Expr(..) )
 
 data ParseError = InvalidUnicodeEscape
   deriving ( Eq, Ord )
@@ -32,42 +32,6 @@ instance ShowErrorComponent ParseError where
       "invalid unicode escape, unicode escape must be at most 0x10ffff"
 
 type Parser = Parsec ParseError Text
-
-data Expr
-  = Int Integer
-  | Nat Natural
-  | Float Double
-  | Char Char
-  | String String
-  | Var String [String]
-  | Unit
-  | Pair Expr Expr
-  | List [Expr]
-  | App Expr Expr
-  | Let Expr Expr Expr
-  | If Expr Expr Expr
-  | Match Expr (NonEmpty (NonEmpty Expr, Maybe Expr, Expr))
-
-instance Show Expr where
-  show (Int x) = show x
-  show (Nat x) = '+' : show x
-  show (Float x) = show x
-  show (Char x) = show x
-  show (String x) = show x
-  show (Var x xs) = foldl ((++) . (++ ".")) x xs
-  show Unit = "()"
-  show (Pair x y) = printf "(%s,%s)" (show x) (show y)
-  show (List x) = show x
-  show (App x y) = printf "(%s %s)" (show x) (show y)
-  show (Let pat val x)
-    = printf "let %s = %s in %s" (show pat) (show val) (show x)
-  show (If cond x y)
-    = printf "if %s then %s else %s" (show cond) (show x) (show y)
-  show (Match x xs) = "match " ++ show x ++ concatMap showArm xs
-    where
-      showArm (pats, guard, val)
-        = printf "\n%s%s -> %s" (concatMap (("| " ++) . show) pats)
-        (maybe "" ((" when " ++) . show) guard) (show val)
 
 test :: IO ()
 test = do
